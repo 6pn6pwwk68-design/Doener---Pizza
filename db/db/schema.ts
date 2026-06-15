9bca0867997c7453b756689da2eb8164190600fc
6pn6pwwk68-design <6pn6pwwk68@privaterelay.appleid.com>
2026-06-15 13:00:50 +0200

    Delete db/db directory

--- db/db/schema.ts
+++
@@ -1,9 +0,0 @@
-import { pgTable, serial, text, timestamp, jsonb } from "drizzle-orm/pg-core";
-
-export const orders = pgTable("orders", {
-  id: serial().primaryKey(),
-  username: text("username").notNull(),
-  items: jsonb("items").notNull(),
-  total: text("total").notNull(),
-  createdAt: timestamp("created_at").defaultNow(),
-});
